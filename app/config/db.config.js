f11224ce86e4dab8a0551ab67568db79fcf516bf
riky <pembuatan api selesai>
2022-03-21 14:58:24 +0700

    push

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    database: node_mysql,
-    username: root,
-    password: rikysaga46,
-    host: localhost,
-    port: 5433,
-    dialect: 'postgres',
+    database: "node_mysql",
+    username: "root",
+    password: "rikysaga46",
+    host: "localhost",
+    port: "5433",
+    dialect: "postgres",
     dialectOptions: {
         ssl: {
             require: true,
