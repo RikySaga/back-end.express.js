f59afec0ec3b2c9d3a75254322cbca42cccb91e5
riky <pembuatan api selesai>
2022-03-21 15:20:38 +0700

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,25 @@
-module.exports = {
-    database: "node_mysql",
-    username: "root",
-    password: "rikysaga46",
-    host: "localhost",
-    port: "5433",
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
+const creds = {
+    development: {
+        username: "rikysaga",
+        password: "rikysaga46",
+        database: "node_sequelize",
+        host: "127.0.0.1",
+        dialect: 'postgresql'
     },
-
-
+    test: {
+        username: "root",
+        password: "rikysaga46",
+        database: "sequelize_test",
+        host: "127.0.0.1",
+        dialect: 'postgresql'
+    },
+    production: {
+        username: "root",
+        password: "rikysaga46",
+        database: "sequelize_test",
+        host: "127.0.0.1",
+        dialect: 'postgresql'
+    }
 };
+
+module.exports = creds;
