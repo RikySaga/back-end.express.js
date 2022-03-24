e4cdba887299d58bdfd485ba7c394cb9eacfd327
riky <pembuatan api selesai>
2022-03-24 14:08:18 +0700

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
+
     HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "rikysaga46",
-    PORT: "5433",
-    DB: "testdb",
-    dialect: "postgres",
+    USER: "root",
+    PASSWORD: "",
+    DB: "node_mysql",
+    dialect: "mysql",
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+        max: 5, // maximum number of connection in pool
+        min: 0, // minimum number of connection in pool
+        acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
+        idle: 10000 // maximum time, in milliseconds, that a connection can be idle before being released
     }
 };
